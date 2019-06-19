f3f80531b06163ae50da42e34c93dce2f473b582
chiameng.ong <chiameng.ong@SIC-SSLT189.sicmsb.com>
2019-06-19 09:02:15 +0800

    Adding encryption to properties value

--- springboot2web/src/main/java/com/garcia/springbootweb/SecurityConfig.java
+++ springboot2web/src/main/java/com/garcia/springbootweb/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.garcia.springbootweb;
 
 import javax.sql.DataSource;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,9 +40,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		//Permit access to h2 console and spring actuator without authentication
+		// Permit access to h2 console and spring actuator without authentication
 		httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests()
-				.antMatchers("/h2-console/**","/actuator/**", "/actuator").permitAll();
+				.antMatchers("/h2-console/**", "/actuator/**", "/actuator").permitAll();
 		httpSecurity.csrf().disable();
 		httpSecurity.headers().frameOptions().disable();
 		super.configure(httpSecurity);
@@ -49,4 +53,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
 	}
 
+	// For application.properties encryption use
+	@Bean(name = "encryptorBean")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword("password");//salt
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+		return encryptor;
+	}
 }
